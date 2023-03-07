a92d49c50961d381cb5710eaa838b4edc42cbeba
unknown <soheil.shafaee.6200@gmail.com>
2023-03-07 23:11:14 +0330

    add home app

---
+++ home/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+STATUS_CHOICES = (
+    ("pub", "published"),
+    ("drf", "draft"))
+
+
+class Price(models.Model):
+    text = models.CharField(max_length=100)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    status = STATUS_CHOICES
