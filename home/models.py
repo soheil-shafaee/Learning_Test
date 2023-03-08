662cf6f3b8b15071785a58e210e45db94bb3a2ea
unknown <soheil.shafaee.6200@gmail.com>
2023-03-08 12:12:52 +0330

    update base file

--- home/models.py
+++ home/models.py
@@ -11,6 +11,7 @@ class Post(models.Model):
     text = models.TextField(max_length=1000)
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     status = models.CharField(choices=STATUS_CHOICES, max_length=3)
+    city = models.CharField(max_length=50)
     datetime_create = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
 
