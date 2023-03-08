ad2f3cc085e26572dfdf6fa1d2141d94b926d602
unknown <soheil.shafaee.6200@gmail.com>
2023-03-08 10:40:35 +0330

    display post table

--- home/models.py
+++ home/models.py
@@ -2,11 +2,16 @@ from django.db import models
 
 STATUS_CHOICES = (
     ("pub", "published"),
-    ("drf", "draft"))
+    ("drf", "draft"),
+)
 
 
 class Post(models.Model):
-    text = models.CharField(max_length=100)
+    title=models.CharField(max_length=100)
+    text = models.TextField(max_length=1000)
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    status = STATUS_CHOICES
+    status = models.CharField(choices=STATUS_CHOICES, max_length=3)
+
+    def __str__(self):
+        return self.title
 
